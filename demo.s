3a14f1530c7a95794ce42f46abd6bf7aa8d742f6
yazuriyamilette <yamilgabriel5663@gmail.com>
2024-03-12 15:35:17 -0400

    Create subroutine to write a background tile. Wrote to screen tiles to be used for the game.

--- demo.s
+++ demo.s
@@ -70,10 +70,326 @@ load_palettes:
   cpx #$20
   bne @loop
 
+backgrounds: ; sets parameters for writeBackground and calls it for each tile we want to write
+  ;first 16x16 tile
+  LDA #$22
+  STA $00
+  LDA #$44
+  STA $01
+  LDA #$02
+  STA $02
+  jsr writeBackground
+
+  LDA #$22
+  STA $00
+  LDA #$45
+  STA $01
+  LDA #$03
+  STA $02
+  jsr writeBackground
+
+  LDA #$22
+  STA $00
+  LDA #$64
+  STA $01
+  LDA #$12
+  STA $02
+  jsr writeBackground
+
+  LDA #$22
+  STA $00
+  LDA #$65
+  STA $01
+  LDA #$13
+  STA $02
+  jsr writeBackground
+
+  ;second 16x16 tile
+  LDA #$22
+  STA $00
+  LDA #$46
+  STA $01
+  LDA #$04
+  STA $02
+  jsr writeBackground
+
+  LDA #$22
+  STA $00
+  LDA #$47
+  STA $01
+  LDA #$05
+  STA $02
+  jsr writeBackground
+
+  LDA #$22
+  STA $00
+  LDA #$66
+  STA $01
+  LDA #$14
+  STA $02
+  jsr writeBackground
+
+  LDA #$22
+  STA $00
+  LDA #$67
+  STA $01
+  LDA #$15
+  STA $02
+  jsr writeBackground
+
+  ; third 16x16 tile
+  LDA #$22
+  STA $00
+  LDA #$48
+  STA $01
+  LDA #$06
+  STA $02
+  jsr writeBackground
+
+  LDA #$22
+  STA $00
+  LDA #$49
+  STA $01
+  LDA #$07
+  STA $02
+  jsr writeBackground
+
+  LDA #$22
+  STA $00
+  LDA #$68
+  STA $01
+  LDA #$16
+  STA $02
+  jsr writeBackground
+
+  LDA #$22
+  STA $00
+  LDA #$69
+  STA $01
+  LDA #$17
+  STA $02
+  jsr writeBackground
+
+  ; fourth 16x16 tile
+  LDA #$22
+  STA $00
+  LDA #$4a
+  STA $01
+  LDA #$08
+  STA $02
+  jsr writeBackground
+
+  LDA #$22
+  STA $00
+  LDA #$4b
+  STA $01
+  LDA #$09
+  STA $02
+  jsr writeBackground
+
+  LDA #$22
+  STA $00
+  LDA #$6a
+  STA $01
+  LDA #$18
+  STA $02
+  jsr writeBackground
+
+  LDA #$22
+  STA $00
+  LDA #$6b
+  STA $01
+  LDA #$19
+  STA $02
+  jsr writeBackground
+
+  ;fifth 16x16 tile
+  LDA #$22
+  STA $00
+  LDA #$4c
+  STA $01
+  LDA #$0a
+  STA $02
+  jsr writeBackground
+
+  LDA #$22
+  STA $00
+  LDA #$4d
+  STA $01
+  LDA #$0b
+  STA $02
+  jsr writeBackground
+
+  LDA #$22
+  STA $00
+  LDA #$6c
+  STA $01
+  LDA #$1a
+  STA $02
+  jsr writeBackground
+
+  LDA #$22
+  STA $00
+  LDA #$6d
+  STA $01
+  LDA #$1b
+  STA $02
+  jsr writeBackground
+
+  ; sixth 16x16 tile
+  LDA #$22
+  STA $00
+  LDA #$4e
+  STA $01
+  LDA #$0c
+  STA $02
+  jsr writeBackground
+
+  LDA #$22
+  STA $00
+  LDA #$4f
+  STA $01
+  LDA #$0d
+  STA $02
+  jsr writeBackground
+
+  LDA #$22
+  STA $00
+  LDA #$6e
+  STA $01
+  LDA #$1c
+  STA $02
+  jsr writeBackground
+
+  LDA #$22
+  STA $00
+  LDA #$6f
+  STA $01
+  LDA #$1d
+  STA $02
+  jsr writeBackground
+
+  ; seventh 16x16 tile
+  LDA #$22
+  STA $00
+  LDA #$50
+  STA $01
+  LDA #$0e
+  STA $02
+  jsr writeBackground
+
+  LDA #$22
+  STA $00
+  LDA #$51
+  STA $01
+  LDA #$0f
+  STA $02
+  jsr writeBackground
+
+  LDA #$22
+  STA $00
+  LDA #$70
+  STA $01
+  LDA #$1e
+  STA $02
+  jsr writeBackground
+
+  LDA #$22
+  STA $00
+  LDA #$71
+  STA $01
+  LDA #$1f
+  STA $02
+  jsr writeBackground
+
+  ; eight 16x16 tile
+
+  LDA #$22
+  STA $00
+  LDA #$52
+  STA $01
+  LDA #$30
+  STA $02
+  jsr writeBackground
+
+  LDA #$22
+  STA $00
+  LDA #$53
+  STA $01
+  LDA #$31
+  STA $02
+  jsr writeBackground
+
+  LDA #$22
+  STA $00
+  LDA #$72
+  STA $01
+  LDA #$40
+  STA $02
+  jsr writeBackground
+
+  LDA #$22
+  STA $00
+  LDA #$73
+  STA $01
+  LDA #$41
+  STA $02
+  jsr writeBackground
+
+
+
+  jmp writeAttributeTables
+
+writeBackground: ; takes a parameter X (high byte), Y(low byte) and index tile to write
+; these parameters are saved in $00, $01, and $02 of the ram respectively
+  LDA PPUSTATUS
+  LDA $00
+  STA PPUADDR
+  LDA $01
+  STA PPUADDR
+  LDX $02
+  STX PPUDATA
+  rts
+
+writeAttributeTables:
+  LDA PPUSTATUS
+  LDA #$23
+  STA PPUADDR
+  LDA #$e1
+  STA PPUADDR
+  LDA #%00000000
+  STA PPUDATA
+
+  LDA PPUSTATUS
+  LDA #$23
+  STA PPUADDR
+  LDA #$e2
+  STA PPUADDR
+  LDA #%10010000
+  STA PPUDATA
+
+  LDA PPUSTATUS
+  LDA #$23
+  STA PPUADDR
+  LDA #$e3
+  STA PPUADDR
+  LDA #%01110000
+  STA PPUDATA
+
+  LDA PPUSTATUS
+  LDA #$23
+  STA PPUADDR
+  LDA #$e4
+  STA PPUADDR
+  LDA #%10100000
+  STA PPUDATA
+
+
+
 enable_rendering:
   lda #%10010000	; Enable NMI
   sta PPUCTRL
-  lda #%00011000	; Enable Sprites and Background
+  lda #%00011010	; Enable sprites, background, and leftmost 8 bits for the background
   sta PPUMASK
 
 forever:
